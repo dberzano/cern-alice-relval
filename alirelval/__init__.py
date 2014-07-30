e8cc78e4657b8f43503a0c5195fd02b081fb1994
Dario Berzano <dario.berzano@cern.ch>
2014-07-30 15:58:25 +0000

    Initialize SQLite status database

--- alirelval/__init__.py
+++ alirelval/__init__.py
@@ -10,6 +10,7 @@ from alipack import AliPack, AliPackError
 import logging, logging.handlers
 import ConfigParser
 from getopt import getopt, GetoptError
+import sqlite3
 
 
 def get_available_packages(baseurl):
@@ -73,27 +74,63 @@ def init_config(config_file):
 
   # 'key': ['type', default]
   config_vars = {
-    'logdir': ['str', '~/.alirelval/log'],
-    'db': ['str', '~/.alirelval/status.sqlite'],
+    'logdir': ['path', '~/.alirelval/log'],
+    'dbpath': ['path', '~/.alirelval/status.sqlite'],
     'packbaseurl': ['str', 'http://pcalienbuild4.cern.ch:8889/tarballs']
   }
 
   for c in config_vars.keys():
+    vartype = config_vars[c][0]
     try:
-      if config_vars[c][0] == 'str':
+      if vartype == 'str' or vartype == 'path':
         config_vars[c] = parser.get(sec, c)
-      elif config_vars[c][0] == 'int':
+      elif vartype == 'int':
         config_vars[c] = parser.getint(sec, c)
-      elif config_vars[c][0] == 'float':
+      elif vartype == 'float':
         config_vars[c] = parser.getfloat(sec, c)
-      elif config_vars[c][0] == 'bool':
+      elif vartype == 'bool':
         config_vars[c] = parser.getboolean(sec, c)
     except Exception:
       config_vars[c] = config_vars[c][1]
+      default = True
+
+    if vartype == 'path':
+      config_vars[c] = os.path.expanduser(config_vars[c])
+    if default:
       log.debug('%s.%s = %s (default)' % (sec, c, config_vars[c]))
-    log.debug('%s.%s = %s (from file)' % (sec, c, config_vars[c]))
+    else:
+      log.debug('%s.%s = %s (from file)' % (sec, c, config_vars[c]))
 
   return config_vars
+
+
+def init_db(dbpath):
+  log = get_logger()
+  log.debug('using database file %s' % dbpath)
+  db = sqlite3.connect(dbpath)
+  cursor = db.cursor()
+  cursor.execute('''
+    CREATE TABLE IF NOT EXISTS package(
+      package_id INT PRIMARY KEY,
+      tarball    TEXT UNIQUE NOT NULL,
+      software   TEXT NOT NULL,
+      version    TEXT NOT NULL,
+      arch       TEXT NOT NULL
+    )
+  ''')
+  cursor.execute('''
+    CREATE TABLE IF NOT EXISTS validation(
+      validation_id INT PRIMARY KEY,
+      started       INT,
+      ended         INT,
+      status        INT NOT NULL,
+      package_id    INT NOT NULL,
+      FOREIGN KEY(package_id) REFERENCES package(package_id)
+    )
+  ''')
+  db.commit()
+  db.close()
+
 
 def list_packages(baseurl):
   log = get_logger()
@@ -144,7 +181,10 @@ def main(argv):
   if debug:
     init_logger(log_directory=None, debug=True)
   cfg = init_config( os.path.expanduser('~/.alirelval/alirelval.conf') )
-  init_logger( log_directory=os.path.expanduser(cfg['logdir']), debug=debug )
+  init_logger( log_directory=cfg['logdir'], debug=debug )
+
+  # init the database
+  init_db(cfg['dbpath'])
 
   # what to do
   if action == 'list-packages':
